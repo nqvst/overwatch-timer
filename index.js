52c5c244bde730e53223570fa27e6333fef047a9
nqvst <jack.enqvist@gmail.com>
2019-12-26 21:13:11 +0100

    fix 0 padd

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ let currentTime = (hour, min, sec) => `${Math.max(1, hour % 3)}:${min}:${sec}`;
 
 setInterval(() => {
   const date = new Date();
-  const newTime = currentTime(
-    date.getHours(),
-    date.getMinutes(),
-    date.getSeconds()
-  );
+  date.setHours(1 + (date.getHours() % 3));
+
   const timeNode = document.getElementById("time");
-  timeNode.innerHTML = `elapsed time: ${newTime}`;
+  timeNode.innerHTML = `elapsed time: ${date.toLocaleTimeString()}`;
 }, 1000);
+
+console.log("%cHello there!", "color:yellow; font-size: 24px");
+console.log(
+  "interested in how this was made? check out the git repository on github: https://github.com/nqvst/overwatch-timer"
+);

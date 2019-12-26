4bbf3d2a5729da40a0ce71ec0f4eefe2869c4c50
nqvst <jack.enqvist@gmail.com>
2019-12-26 20:08:16 +0100

    Add a basic fake timer

---
+++ index.js
@@ -0,0 +1,12 @@
+let currentTime = (hour, min, sec) => `${Math.max(1, hour)}:${min}:${sec}`;
+
+setInterval(() => {
+  const date = new Date();
+  const newTime = currentTime(
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds()
+  );
+  const timeNode = document.getElementById("time");
+  timeNode.innerHTML = `elapsed time: ${newTime}`;
+}, 1000);
